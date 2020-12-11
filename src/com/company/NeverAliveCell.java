4cb463f622d7e2d7e74392d857007672b8e6cf56
nyccowgirl <nyc_cowgirl@hotmail.com>
2020-12-10 18:23:43 -0600

    Initial commit

---
+++ src/com/company/NeverAliveCell.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class NeverAliveCell extends AbstractCell {
+
+    public NeverAliveCell(int r, int c, ConwayWorld w) {
+        super(r, c, w);
+    }
+
+    public  AbstractCell cellForNextGeneration() {
+        return this;
+    }
+
+    public boolean willBeAliveInNextGeneration() {
+        return false;
+    }
+
+    public char displayCharacter() {
+        return 'X';
+    }
+
+    public boolean getIsAlive() {
+        return false;
+    }
+}
